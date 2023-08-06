f996d054145d5b9058b0b5ebd1db3919bec3253b
vinayak krishnanRR <vinayakrr67@gmail.com>
2023-08-06 12:09:22 +0530

    first commit

---
+++ basics.js
@@ -0,0 +1,53 @@
+console.log('welcome to js');
+var a=10
+var b=20
+var result=a+b;
+console.log(result);
+var firstname=2
+console.log(firstname);
+{
+const a=98;
+console.log(a);
+// /let b=65;
+// let result=a+b;
+// console.log(result);
+}
+// console.log(result);
+{
+    const a=90
+    var b=90
+    const result=a+b;
+    console.log(result);
+}
+array1=['cat','dog','elephant','camel'];
+console.log(array1);
+var name='vinayak';
+console.log(typeof(name));
+b=false
+console.log(typeof(b));
+var a=55
+ var b=67
+ var result=a%b;
+ console.log(result)
+// var a=1
+// var b=a++;
+// console.log(a);
+var a=10;
+var result=a++;
+console.log(a);
+var firstname='vinayak';
+var secondname='sabari';
+var result=firstname+58;
+console.log(result);
+// objects in javascript
+
+// let person={
+    // address:{
+        // place:'medyilmukku',
+        // city:'kollam',
+        // state:'kerala',
+    // }
+// }
+// console.log(person.address.state);
+var array1=['apple','orange','grapes','pineapple'];
+console.log(array1.indexof('apple'));
